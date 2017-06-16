fa62575f6c1c72ce13eccdf7fbb166ded39ded20
Nick Sweet <nick@silo.io>
2017-06-16 15:26:51 -0700

    clink delegate protocol declaration

---
+++ Clink/Classes/ClinkDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  ClinkDelegate.swift
+//  clink
+//
+//  Created by Nick Sweet on 6/16/17.
+//
+
+import Foundation
+
+public protocol ClinkDelegate: class {
+    func clink(_ clink: Clink, didDiscoverPeer peer: ClinkPeer)
+    func clink(_ clink: Clink, didConnectPeer peer: ClinkPeer)
+    func clink(_ clink: Clink, didDisconnectPeer peer: ClinkPeer)
+    func clink(_ clink: Clink, didUpdateDataForPeer peer: ClinkPeer)
+    func clink(_ clink: Clink, didCatchError: Error)
+}
