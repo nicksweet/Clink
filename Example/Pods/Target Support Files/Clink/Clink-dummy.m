21d599bbf07d017860b72d175c690007a25199c6
Nick Sweet <nick@silo.io>
2017-06-16 14:58:17 -0700

    adds pod boilerplate

---
+++ Example/Pods/Target Support Files/Clink/Clink-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Clink : NSObject
+@end
+@implementation PodsDummy_Clink
+@end
---
+++ Example/Pods/Target Support Files/Pods-Clink_Tests/Pods-Clink_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Clink_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Clink_Tests
+@end
