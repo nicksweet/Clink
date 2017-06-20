21dfaf438c0be3afff30ccf3d64716f7ff439420
Nick Sweet <nick@silo.io>
2017-06-20 14:29:03 -0700

    make message start / end flags global vars

--- Clink/Classes/globals.swift
+++ Clink/Classes/globals.swift
@@ -9,4 +9,6 @@ import Foundation
 
 internal var q = DispatchQueue(label: "clink-queue")
 internal let savedPeerIdsDefaultsKey = "clink-tracked-peer-ids"
+internal let messageStartMarker = "START"
+internal let messageEndMarker = "END"
 
