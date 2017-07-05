5200bbb2ad8cd4842eab190b747b75de49066d87
Nick Sweet <nick@silo.io>
2017-07-05 12:52:14 -0400

    track discovered peripheral RSSI / discover service characteristics

--- Clink/Classes/PairingService.swift
+++ Clink/Classes/PairingService.swift
@@ -9,16 +9,29 @@ import Foundation
 import CoreBluetooth
 
 class PairingService: NSObject {
-    var serviceId = "7D912F17-0583-4A1A-A499-205FF6835514"
+    var serviceId = CBUUID(string: "7D912F17-0583-4A1A-A499-205FF6835514")
     var remotePeripheral: CBPeripheral? = nil
     var remotePeripheralRSSI = -999
 }
 
 extension PairingService: CBPeripheralDelegate {
     public func peripheral(_ peripheral: CBPeripheral, didReadRSSI RSSI: NSNumber, error: Error?) {
-        guard peripheral == remotePeripheral?.identifier else { return }
+        guard peripheral.identifier == remotePeripheral?.identifier else { return }
         
         remotePeripheralRSSI = RSSI.intValue
+        
+        peripheral.readRSSI()
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if
+            peripheral.identifier == remotePeripheral?.identifier,
+            let services = peripheral.services,
+            let serviceIndex = services.index(where: { $0.uuid == self.serviceId })
+        {
+            peripheral.discoverCharacteristics(nil, for: services[serviceIndex])
+        }
+        
     }
 }
 
@@ -34,8 +47,11 @@ extension PairingService: CBCentralManagerDelegate {
         rssi RSSI: NSNumber)
     {
         if remotePeripheral == nil {
-            peripheral.delegate = self
             remotePeripheral = peripheral
+            remotePeripheralRSSI = RSSI.intValue
+            
+            peripheral.delegate = self
+            peripheral.readRSSI()
         }
     }
     
