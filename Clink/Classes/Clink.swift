448e1cfd501b970ba6de84e56b13f05b9d999db3
Nick Sweet <nick@silo.io>
2017-07-07 09:54:14 -0400

    remove default peer manager implementation - extend clink to conform to the protocol instead

--- Clink/Classes/Clink.swift
+++ Clink/Classes/Clink.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreBluetooth
 
 
-public class Clink: NSObject, ClinkPeerManager {
+public class Clink: NSObject {
     static public let shared = Clink()
     
     weak public var peerManager: ClinkPeerManager? = nil
@@ -398,4 +398,35 @@ extension Clink: PairingTaskDelegate {
         }
     }
 }
+
+
+// MARK: - PEER MANAGER DELEGATE METHODS
+
+extension Clink: ClinkPeerManager {
+    public func save(peer: Clink.Peer) {
+        UserDefaults.standard.set(peer.toDict(), forKey: peer.id.uuidString)
+        
+        var savedPeerIds = UserDefaults.standard.stringArray(forKey: savedPeerIdsDefaultsKey) ?? []
+        
+        if savedPeerIds.index(of: peer.id.uuidString) == nil {
+            savedPeerIds.append(peer.id.uuidString)
+            UserDefaults.standard.set(savedPeerIds, forKey: savedPeerIdsDefaultsKey)
+        }
+    }
+    
+    public func getSavedPeer(withId peerId: UUID) -> Clink.Peer? {
+        guard let peerDict = UserDefaults.standard.dictionary(forKey: peerId.uuidString) else { return nil }
+        
+        return Clink.Peer(dict: peerDict)
+    }
+    
+    public func getSavedPeers() -> [Clink.Peer] {
+        return (UserDefaults.standard.stringArray(forKey: savedPeerIdsDefaultsKey) ?? []).flatMap { uuidString in
+            guard let uuid = UUID(uuidString: uuidString) else { return nil }
+            
+            return self.getSavedPeer(withId: uuid)
+        }
+    }
+}
+
 
