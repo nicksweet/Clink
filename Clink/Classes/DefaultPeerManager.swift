94e32bc08a3ab8c598cd078a298e4fde0d752d46
Nick Sweet <nick@silo.io>
2017-07-13 12:20:49 -0400

    implements default peer manager

---
+++ Clink/Classes/DefaultPeerManager.swift
@@ -0,0 +1,52 @@
+//
+//  DefaultPeerManager.swift
+//  Clink
+//
+//  Created by Nick Sweet on 7/13/17.
+//
+
+import Foundation
+
+
+public class DefaultPeerManager: ClinkPeerManager {
+    public typealias RemotePeer = Clink.Peer
+    
+    public func createPeer(withId peerId: String) -> RemotePeer {
+        let peer = Clink.Peer(id: peerId)
+        
+        UserDefaults.standard.set(peer.toDict(), forKey: peer.id)
+        
+        var savedPeerIds = UserDefaults.standard.stringArray(forKey: savedPeerIdsDefaultsKey) ?? []
+        
+        if savedPeerIds.index(of: peer.id) == nil {
+            savedPeerIds.append(peer.id)
+            UserDefaults.standard.set(savedPeerIds, forKey: savedPeerIdsDefaultsKey)
+        }
+        
+        return peer
+    }
+    
+    public func update(peer: RemotePeer, with data: [String: Any]) {
+        var clinkPeer = peer
+        
+        clinkPeer.data = data
+        
+        UserDefaults.standard.set(clinkPeer.toDict(), forKey: clinkPeer.id)
+    }
+    
+    public func getPeer(withId peerId: String) -> RemotePeer? {
+        guard let peerDict = UserDefaults.standard.dictionary(forKey: peerId) else { return nil }
+        
+        return Clink.Peer(dict: peerDict)
+    }
+    
+    public func getKnownPeers() -> [RemotePeer] {
+        return (UserDefaults.standard.stringArray(forKey: savedPeerIdsDefaultsKey) ?? []).flatMap { peerId in
+            return self.getPeer(withId: peerId)
+        }
+    }
+    
+    public func delete(peer: RemotePeer) {
+        UserDefaults.standard.removeObject(forKey: peer.id)
+    }
+}
