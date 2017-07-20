dd4eff2c5ba01b7d95a5d4879f605fa9d02da579
Nick Sweet <nick@silo.io>
2017-07-20 11:53:11 -0400

    init configuration peer manager static with default peer manager

--- Clink/Classes/NestedTypes.swift
+++ Clink/Classes/NestedTypes.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension Clink {
     public struct Configuration {
-        public static var peerManager: ClinkPeerManager? = nil
+        public static var peerManager: ClinkPeerManager = DefaultPeerManager()
         public static var dispatchQueue = DispatchQueue(label: "clink-queue")
     }
     
