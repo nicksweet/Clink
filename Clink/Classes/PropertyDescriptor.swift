88785e6cc364f4853ae00678bedc36e15328cbf0
Nick Sweet <nick@silo.io>
2017-07-27 13:20:09 -0400

    add custom string convertible description to property descriptor / refactor

--- Clink/Classes/PropertyDescriptor.swift
+++ Clink/Classes/PropertyDescriptor.swift
@@ -7,41 +7,41 @@
 
 import Foundation
 
-extension Clink {
-    internal class PropertyDescriptor: NSObject, NSCoding {
-        let name: Clink.PeerPropertyKey
-        let value: Any
-        let characteristicId: String
-        
-        init(name: String, value: Any, characteristicId: String) {
-            self.name = name
-            self.value = value
-            self.characteristicId = characteristicId
-            
-            super.init()
-        }
-        
+
+internal class PropertyDescriptor: NSObject, NSCoding {
+    let name: Clink.PeerPropertyKey
+    let value: Any
+    let characteristicId: String
+    
+    override var description: String {
+        return "name: \(name), value: \(value), characteristicId: \(characteristicId)"
     }
     
-    extension PropertyDescriptor: NSCoding {
-        required init?(coder aDecoder: NSCoder) {
-            guard
-                let name = aDecoder.decodeObject(forKey: "name") as? String,
-                let value = aDecoder.decodeObject(forKey: "name"),
-                let characteristicId = aDecoder.decodeObject(forKey: "characteristicId") as? String
-                else {
-                    return nil
-            }
-            
-            self.name = name
-            self.value = value
-            self.characteristicId = characteristicId
+    init(name: Clink.PeerPropertyKey, value: Any, characteristicId: String) {
+        self.name = name
+        self.value = value
+        self.characteristicId = characteristicId
+        
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        guard
+            let name = aDecoder.decodeObject(forKey: "name") as? String,
+            let value = aDecoder.decodeObject(forKey: "name"),
+            let characteristicId = aDecoder.decodeObject(forKey: "characteristicId") as? String
+            else {
+                return nil
         }
         
-        func encode(with aCoder: NSCoder) {
-            aCoder.encode(name, forKey: "name")
-            aCoder.encode(value, forKey: "value")
-            aCoder.encode(characteristicId, forKey: "characteristicId")
-        }
+        self.name = name
+        self.value = value
+        self.characteristicId = characteristicId
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(value, forKey: "value")
+        aCoder.encode(characteristicId, forKey: "characteristicId")
     }
 }
