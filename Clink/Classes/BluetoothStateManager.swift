72500e995f695c0d1af9e46f97d9fe7181d7d3d0
Nick Sweet <nick@silo.io>
2017-08-06 10:38:44 -0400

    fix peripheral manager state check timer not firing

--- Clink/Classes/BluetoothStateManager.swift
+++ Clink/Classes/BluetoothStateManager.swift
@@ -18,17 +18,20 @@ extension BluetoothStateManager {
     func once(manager: CBManager, hasState state: CBManagerState, invoke block: @escaping (Clink.Result<Void>) -> Void) {
         if manager.state == state { return block(.success(result: ())) }
         
-        var attempts = 0
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-            attempts += 1
+        DispatchQueue.main.async {
+            var attempts = 0
             
-            if manager.state == state {
-                timer.invalidate()
-                return block(Clink.Result.success(result: ()))
-            } else if attempts > 5 {
-                timer.invalidate()
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                attempts += 1
                 
-                return block(.error(.managerFailedToAchieveState))
+                if manager.state == state {
+                    timer.invalidate()
+                    return block(Clink.Result.success(result: ()))
+                } else if attempts > 5 {
+                    timer.invalidate()
+                    
+                    return block(.error(.managerFailedToAchieveState))
+                }
             }
         }
     }
