7936f207435091cf7ad7a6dd7e8720747c413384
Nick Sweet <nick@silo.io>
2017-08-08 17:16:42 -0400

    declare "read operation class"

--- Clink/Classes/ReadOpperation.swift
+++ Clink/Classes/ReadOpperation.swift
@@ -15,3 +15,13 @@ private let endOfMessageFlag = "EOM"
 internal protocol ReadOpperationDelegate {
     func readOpperation(opperation: ReadOpperation, didCompleteWithPropertyDescriptor descriptor: PropertyDescriptor?)
 }
+
+internal class ReadOpperation {
+    public let peripheral: CBPeripheral
+    
+    private var packets = [Data]()
+    
+    init(peripheral: CBPeripheral) {
+        self.peripheral = peripheral
+    }
+}
